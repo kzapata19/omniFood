8e78d7c88aa4e4e4f96cac1aef3e1fe1f0db7ec8
Karen Zapata <kzapata0919@gmail.com>
2017-08-04 19:56:11 -0700

    Add waypoint method to activate 'sticky' class when page is scrolled down

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
 
-
+  $('.js--section-features').waypoint(function(direction){
+      if(direction == "down") {
+        $('nav').addClass('sticky');
+      } else {
+        $('nav').remove('sticky');
+      }
+  }, { offset: '60px' });
 
 });
