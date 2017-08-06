68728167275b371d20daa90ab494679f512ddb4c
Karen Zapata <kzapata0919@gmail.com>
2017-08-06 13:15:58 -0700

    Add classes for "I'm hungry" and "Show me more" buttons
    Enable smooth page scrolling to plans section and features section respectively

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,11 +1,17 @@
 $(document).ready(function(){
 
+// For the sticky nav
   $('.js--section-features').waypoint(function(direction){
       if(direction == "down") {
         $('nav').addClass('sticky');
       } else {
-        $('nav').remove('sticky');
+        $('nav').removeClass('sticky');
       }
   }, { offset: '60px' });
 
+// Scroll on buttons
+
+  $('.js--scroll-to-plans').click(function(){
+      $('html, body').animate({scrollTop: $('.js--section-plans').offset().top}, 1000);
+  });
 });
