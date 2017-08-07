a53a75627305f232e27178791de78cf62548acad
Karen Zapata <kzapata0919@gmail.com>
2017-08-06 18:07:30 -0700

    Add close icon
    On click, switch between 'stack' and 'close' menu icons

--- resources/js/script.js
+++ resources/js/script.js
@@ -88,8 +88,17 @@ $(document).ready(function(){
 
   $('.js--nav-icon').click(function(){
     const nav = $('.js--main-nav');
+    const icon = $('.js--nav-icon i')
 
     nav.slideToggle(200);
+
+    if(icon.hasClass('ion-navicon-round')){
+      icon.addClass('ion-close-round');
+      icon.removeClass('ion-navicon-round');
+    } else {
+      icon.removeClass('ion-close-round');
+      icon.addClass('ion-navicon-round');
+    }
 
   });
 
